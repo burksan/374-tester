8bdd5a92a3f1c776c49ae89ff3d684795ddca458
Austin Cory Bart <acbart@vt.edu>
2020-02-20 23:28:28 -0500

    Updates

---
+++ dev/assets/citations.md
@@ -0,0 +1,3 @@
+*Please be sure to list the source of all assets you used here.*
+
+*At least a URL, but also be sure to include any extra information that the artist/author requests you include.*
---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-phaser-game-template/devprecache-manifest.549ef5a400021d602b1a18ffdc9f4103.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
